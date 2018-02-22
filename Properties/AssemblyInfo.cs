c2b4bd1eb6a21b39f24652188228c371f14ff960
Dooks123 <Dooks123@gmail.com>
2018-02-22 12:15:32 +0200

    Initial Commit of the project

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Android.App;
+
+[assembly: AssemblyTitle("Android_Job_XamarinBinding")]
+[assembly: AssemblyDescription("Xamarin Binding for Android-Job (by Evernote). Compiled by Dooks123")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Dooks123")]
+[assembly: AssemblyProduct("Android-Job-XamarinBinding")]
+[assembly: AssemblyCopyright("OpenLicense")]
+[assembly: AssemblyTrademark("Evernote")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
